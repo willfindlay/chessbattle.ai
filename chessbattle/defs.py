227fca507eb8b35d22b8631ad83d386723bbe3b5
willfindlay <william@williamfindlay.com>
2020-05-14 12:02:49 -0400

    Fix some problems with serving static files

--- chessbattle/defs.py
+++ chessbattle/defs.py
@@ -1,3 +1,4 @@
 import os
 
 PROJECT_ROOT = os.path.join(os.path.dirname(__file__), '..')
+STATIC_PATH = os.path.join(PROJECT_ROOT, 'static')
--- chessbattle/server.py
+++ chessbattle/server.py
@@ -1,14 +1,30 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 
-from chessbattle.utils import static_path
+from chessbattle.defs import STATIC_PATH
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
 def index():
-    with open(static_path('index.html'), 'r') as f:
-        return f.read()
+    return send_from_directory(STATIC_PATH, 'index.html')
+
+
+@app.route('/<path:path>')
+def path(path):
+    return send_from_directory(STATIC_PATH, path)
+
+
+#
+#
+# @app.route('/css/<path:path>', methods=['GET'])
+# def css(path):
+#    return send_from_directory(static_path('css'), path)
+#
+#
+# @app.route('/js/<path:path>', methods=['GET'])
+# def js(path):
+#    return send_from_directory(static_path('js'), path)
 
 
 def main():
